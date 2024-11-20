0e665902a974c55e5f1e64ee5ef3a165b7635f5d
Zachary Williams <zjwilliams@proton.me>
2024-11-19 22:05:42 -0600

    dumb mistake

--- build.sh
+++ build.sh
@@ -83,7 +83,7 @@ fi
 # Patch the Dockerfile to work with podman (Unsure why it works with docker, by appearances it shouldn't)
 cat azerothcore-wotlk/apps/docker/Dockerfile | \
         sed 's/ # lts//g' | \
-	sed 's/--mount=type//g' | \
+	sed 's/--mount=type/#--mount=type/g' | \
 	sed 's/FROM skeleton AS client-data/FROM runtime AS client-data\n\nUSER 0/g' | \
 	sed 's/$DOCKER_USER:$DOCKER_USER/$USER_ID:$GROUP_ID/g' | \
 	sed 's/USER $DOCKER_USER//g' | \
